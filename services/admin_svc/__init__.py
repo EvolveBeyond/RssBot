5bf61a57bec8d07ccc4d5afe162a94f5999d9a5c
Pakrohk <rhpcir@gmail.com>
2025-12-16 03:27:15 +0330

    Move to new atch

--- services/admin_svc/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Admin Service - Service registry management and monitoring dashboard
-"""
--- src/rssbot/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-RSS Bot Models Package
-"""
-
-from .service_registry import RegisteredService, ConnectionMethod
-
-__all__ = ["RegisteredService", "ConnectionMethod"]
